05766e948ee8fe94d136473f68aa24daeae94027
emekadefirst <victorchibuogwu33@gmail.com>
2025-10-03 17:30:56 +0100

    rege

--- src/scripts/server.py
+++ src/scripts/server.py
@@ -53,10 +53,19 @@ register_tortoise(
 )
 
 app.add_middleware(GZipMiddleware, minimum_size=1000)
+allowed_origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+    "http://127.0.0.1:5173",
+    "https://fhamortgage.gov.ng"
+]
+
 app.add_middleware(
     CORSMiddleware,
-    # allow_origins=CORS_ALLOWED_ORIGINS,
-    allow_origins=["*"],
+    allow_origins=allowed_origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
